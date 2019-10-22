27ae411e46bc233e7b116f71b819034e6625d555
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-10-22 13:01:23 +0200

    [feature] Created widget to render a title context and thumbnail for a project

--- controls/includes/class-djc-elements-controls-projects.php
+++ controls/includes/class-djc-elements-controls-projects.php
@@ -1,8 +1,8 @@
 <?php
 
-use Elementor\Base_Control;
+use Elementor\Base_Data_Control;
 
-class Djc_Elements_Controls_Projects extends Base_Control {
+class Djc_Elements_Controls_Projects extends Base_Data_Control {
     public static $type = 'project-loader';
     
     public function get_type(): string {
@@ -10,17 +10,22 @@ class Djc_Elements_Controls_Projects extends Base_Control {
     }
     
     public function content_template() {
-        $control_uid = 'er';
+        $control_uid = $this->get_control_uid();
+        $projects = $this->get_projects();
         ?>
         <div class="elementor-control-field">
             <label for="<?php echo esc_attr( $control_uid ); ?>" class="elementor-control-title">{{{ data.label }}}</label>
             <div class="elementor-control-input-wrapper">
-                <textarea id="<?php echo esc_attr( $control_uid ); ?>" class="elementor-control-tag-area" rows="{{ data.rows }}" data-setting="{{ data.name }}" placeholder="{{ data.placeholder }}"></textarea>
+                <?php var_dump($projects); ?>
             </div>
         </div>
         <# if ( data.description ) { #>
         <div class="elementor-control-field-description">{{{ data.description }}}</div>
         <# } #>
         <?php
+    }
+    
+    protected function get_projects() {
+        return get_posts(['post_type' => 'projecten']);
     }
 }
--- widgets/includes/class-djc-elements-widgets-cta.php
+++ widgets/includes/class-djc-elements-widgets-cta.php
@@ -23,16 +23,75 @@ class Djc_Elements_Widgets_CTA extends Widget_Base {
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
+    
+        $this->add_group_control(
+            \ElementorPro\Modules\QueryControl\Controls\Group_Control_Query::get_type(),
+            [
+                'name' => $this->get_name(),
+                'presets' => [ 'full' ],
+                'exclude' => [
+                    'posts_per_page', //use the one from Layout section
+                ],
+            ]
+        );
         
-        $this->add_control( 'project', [
-            'label'     => 'Project',
-            'type'      => Djc_Elements_Controls_Projects::$type
-        ]);
+        $this->add_control(
+            'reverse',
+            [
+                'label'     => __('Reverse', 'djc-elements'),
+                'type'      => \Elementor\Controls_Manager::SWITCHER
+            ]
+        );
     
         $this->end_controls_section();
     }
     
     protected function _register_controls(): void {
         $this->register_content_controls();
+    }
+    
+    protected function render() {
+        $settings = $this->get_settings();
+        $id = $settings['project-cta_posts_ids'][0];
+        $reverse = (bool) $settings['reverse'];
+        ?>
+        <article class="related-project-banner">
+            <?php if ($reverse): ?>
+                <?php $this->image_render($id); ?>
+                <?php $this->content_render($id); ?>
+            <?php else: ?>
+                <?php $this->content_render($id); ?>
+                <?php $this->image_render($id); ?>
+            <?php endif; ?>
+        </article>
+        <?php
+    }
+    
+    /**
+     * @param int $id
+     */
+    protected function content_render($id) {
+        add_filter('excerpt_more','__return_false');
+    
+        $title = get_the_title($id);
+        $excerpt = get_the_excerpt($id);
+        ?>
+        <main class="related-project-content">
+            <h2 class="related-project-title"><?=$title?></h2>
+            <p class="related-project-excerpt">
+                <?=$excerpt?>
+            </p>
+        </main>
+        <?php
+    }
+    
+    protected function image_render($id) {
+        $title = get_the_title($id);
+        $thumbnail = get_the_post_thumbnail_url($id);
+        ?>
+        <figure class="related-project-figure">
+            <img src="<?=$thumbnail?>" alt="<?=$title?>" class="related-project-image" />
+        </figure>
+        <?php
     }
 }
